4ef72fc48efe37af570227a903b4bd08e1ef35bc
Jen-Heard <jlheard11@gmail.com>
2024-12-08 13:32:13 -0600

    Deleted and retried all steps

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-var startMyDay = function() {
-    return "Time for coffee and a puzzle!";
+var startMyDay = function () {
+    return `Time for coffee and a puzzle!`;
 };
 console.log(startMyDay());
 
-var favoriteCookie = function(cookie) {
-    return `My favorite cookie is ${cookie}`;
-}
-console.log(favoriteCookie());
-console.log(favoriteCookie("white chocolate macadamia nut"));
+var favoriteCookie = function (cookie) {
+    return `My favorite cookie is ${cookie}.`;
+};
+console.log(favoriteCookie("chocolate chip"));
 
-var introduce = function(name, occupation) {
-    return `My name is ${name}, and I am a ${occupation}`;
-}
+var introduce = function (name, occupation) {
+    return `Hi! My name is ${name}, and I am a ${occupation}.`;
+};
+
 console.log(introduce("Jen", "front end developer"));
+console.log(introduce("Tabor", "marketing guru"));
 
-var hydrationFeedback = function() {
+var hydrationFeedback = function (){
     var numGlasses = Number(prompt("How many glasses of water did you have today?"));
-    if (numGlasses >=8) {
-        console.log("Congratulations on meeting your water goals!")
+    if (numGlasses >= 8) {
+        console.log(`Congratulations on staying hyrdrated!`);
     } else {
-        console.log("Not bad, but try to drink at least 8 glasses tomorrow!")
+        console.log(`Not bad, but let's try for a bit more tomorrow!`);
     }
-    };
+};
 
 hydrationFeedback();
